e4bc4c8c51a7ba53459556dd233b7adc35c9174a
pallav <pallavk@gmail.com>
2019-10-17 18:12:47 +0800

    Initial commit of the Learning Angular journey

---
+++ APM-Start/src/app/products/product.ts
@@ -0,0 +1,11 @@
+// Product interface definition
+export interface IProduct {
+    productId: number;
+    productName: string;
+    productCode: string; 
+    releaseDate:string;
+    price: number;
+    description: string;
+    starRating: number;
+    imageUrl: string;
+}
